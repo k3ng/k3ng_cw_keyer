ede0be30a748ed56c67b6ef4dc90e583f8d423aa
Anthony Good <anthony.good@gmail.com>
2015-02-02 23:00:21 -0500

    define OPTION_PS2_KEYBOARD_US made default

--- K3NG_PS2Keyboard.h
+++ K3NG_PS2Keyboard.h
@@ -34,9 +34,9 @@
 #ifndef K3NG_PS2Keyboard_h
 #define K3NG_PS2Keyboard_h
 
-//#define OPTION_PS2_KEYBOARD_US
+#define OPTION_PS2_KEYBOARD_US
 //#define OPTION_PS2_KEYBOARD_GERMAN
-#define OPTION_PS2_KEYBOARD_FRENCH
+//#define OPTION_PS2_KEYBOARD_FRENCH
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
