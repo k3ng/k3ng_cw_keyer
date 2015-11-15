d92b19832076f28045d6fb456cabe09138251385
Anthony Good <anthony.good@gmail.com>
2015-11-15 18:17:02 -0500

    2.2.2015111501       Fixed storage of KN prosign in memory (Thank Stefan, DL1SMF)

--- k3ng_keyer.ino
+++ k3ng_keyer.ino
@@ -427,11 +427,14 @@ New fetures in this stable release:
       K3NG_PS2Keyboard Library: Fixed issues with CTRL and ALT key combinations with German and French keyboards 
 
     2.2.2015101801
-      OPTION_WINKEY_IGNORE_LOWERCASE    
+      OPTION_WINKEY_IGNORE_LOWERCASE  
+
+    2.2.2015111501
+      Fixed storage of KN prosign in memory (Thank Stefan, DL1SMF)  
       
 */
 
-#define CODE_VERSION "2.2.2015101801"
+#define CODE_VERSION "2.2.2015111501"
 #define eeprom_magic_number 19
 
 #include <stdio.h>
@@ -9106,110 +9109,107 @@ void serial_status(HardwareSerial * port_to_use) {
 int convert_cw_number_to_ascii (long number_in)
 {
 
- switch (number_in) {
-   case 12: return 65; break;         // A
-   case 2111: return 66; break;
-   case 2121: return 67; break;
-   case 211: return 68; break;
-   case 1: return 69; break;
-   case 1121: return 70; break;
-   case 221: return 71; break;
-   case 1111: return 72; break;
-   case 11: return 73; break;
-   case 1222: return 74; break;
-   case 212: return 75; break;
-   case 1211: return 76; break;
-   case 22: return 77; break;
-   case 21: return 78; break;
-   case 222: return 79; break;
-   case 1221: return 80; break;
-   case 2212: return 81; break;
-   case 121: return 82; break;
-   case 111: return 83; break;
-   case 2: return 84; break;
-   case 112: return 85; break;
-   case 1112: return 86; break;
-   case 122: return 87; break;
-   case 2112: return 88; break;
-   case 2122: return 89; break;
-   case 2211: return 90; break;    // Z
+  switch (number_in) {
+    case 12: return 65; break;         // A
+    case 2111: return 66; break;
+    case 2121: return 67; break;
+    case 211: return 68; break;
+    case 1: return 69; break;
+    case 1121: return 70; break;
+    case 221: return 71; break;
+    case 1111: return 72; break;
+    case 11: return 73; break;
+    case 1222: return 74; break;
+    case 212: return 75; break;
+    case 1211: return 76; break;
+    case 22: return 77; break;
+    case 21: return 78; break;
+    case 222: return 79; break;
+    case 1221: return 80; break;
+    case 2212: return 81; break;
+    case 121: return 82; break;
+    case 111: return 83; break;
+    case 2: return 84; break;
+    case 112: return 85; break;
+    case 1112: return 86; break;
+    case 122: return 87; break;
+    case 2112: return 88; break;
+    case 2122: return 89; break;
+    case 2211: return 90; break;    // Z
 
-   case 22222: return 48; break;    // 0
-   case 12222: return 49; break;
-   case 11222: return 50; break;
-   case 11122: return 51; break;
-   case 11112: return 52; break;
-   case 11111: return 53; break;
-   case 21111: return 54; break;
-   case 22111: return 55; break;
-   case 22211: return 56; break;
-   case 22221: return 57; break;
-   case 112211: return '?'; break;  // ?
-   case 21121: return 47; break;   // /
-   case 2111212: return '*'; break; // BK   
-   case 221122: return 44; break;  // ,
-   case 121212: return '.'; break;
-   case 122121: return '@'; break;
-   case 222222: return 92; break;  // special hack; six dahs = \ (backslash)
-   //case 2222222: return '+'; break;
-   case 9: return 32; break;       // special 9 = space
+    case 22222: return 48; break;    // 0
+    case 12222: return 49; break;
+    case 11222: return 50; break;
+    case 11122: return 51; break;
+    case 11112: return 52; break;
+    case 11111: return 53; break;
+    case 21111: return 54; break;
+    case 22111: return 55; break;
+    case 22211: return 56; break;
+    case 22221: return 57; break;
+    case 112211: return '?'; break;  // ?
+    case 21121: return 47; break;   // /
+    case 2111212: return '*'; break; // BK   
+    case 221122: return 44; break;  // ,
+    case 121212: return '.'; break;
+    case 122121: return '@'; break;
+    case 222222: return 92; break;  // special hack; six dahs = \ (backslash)
+    //case 2222222: return '+'; break;
+    case 9: return 32; break;       // special 9 = space
    
-   #ifndef OPTION_PS2_NON_ENGLISH_CHAR_LCD_DISPLAY_SUPPORT
-   case 21112: return '='; break;
-   case 12121: return '+'; break;
-   #else
-   case 211112: return 45; break; // - // sp5iou
-   case 21112: return 61; break; // = //sp5iou
-   case 212122: return 33; break; // ! //sp5iou
-   //case 121212: return 46; break; // . //sp5iou
-   case 1112112: return 36; break; // $ //sp5iou
-   case 12111: return 38; break; // & // sp5iou
-   //case 221122: return 44; break; // ,
-   case 122221: return 39; break; // ' // sp5iou
-   case 121121: return 34; break; // " // sp5iou
-   case 112212: return 95; break; // _ // sp5iou
-   case 212121: return 59; break; // ; // sp5iou
-   case 222111: return 58; break; // : // sp5iou
-   case 212212: return 41; break; // KK (stored as ascii ) ) // sp5iou
-   case 111212: return 62; break; // SK (stored as ascii > ) // sp5iou
-   case 12121: return 60; break; // AR (store as ascii < ) // sp5iou
-   case 21221: return 40; break; // (KN store as ascii ( ) //sp5iou
-   #endif //OPTION_PS2_NON_ENGLISH_CHAR_LCD_DISPLAY_SUPPORT
+    #ifndef OPTION_PS2_NON_ENGLISH_CHAR_LCD_DISPLAY_SUPPORT
+      case 21112: return '='; break;
+      case 12121: return '+'; break;
+    #else
+      case 211112: return 45; break; // - // sp5iou
+      case 21112: return 61; break; // = //sp5iou
+      case 212122: return 33; break; // ! //sp5iou
+      //case 121212: return 46; break; // . //sp5iou
+      case 1112112: return 36; break; // $ //sp5iou
+      case 12111: return 38; break; // & // sp5iou
+      //case 221122: return 44; break; // ,
+      case 122221: return 39; break; // ' // sp5iou
+      case 121121: return 34; break; // " // sp5iou
+      case 112212: return 95; break; // _ // sp5iou
+      case 212121: return 59; break; // ; // sp5iou
+      case 222111: return 58; break; // : // sp5iou
+      case 212212: return 41; break; // KK (stored as ascii ) ) // sp5iou
+      case 111212: return 62; break; // SK (stored as ascii > ) // sp5iou
+      case 12121: return 60; break; // AR (store as ascii < ) // sp5iou
+    #endif //OPTION_PS2_NON_ENGLISH_CHAR_LCD_DISPLAY_SUPPORT
+
+    case 21221: return 40; break; // (KN store as ascii ( ) //sp5iou //aaaaaaa
+
+    #ifdef OPTION_NON_ENGLISH_EXTENSIONS
+      // for English/Cyrillic/Western European font LCD controller (HD44780UA02):
+      case 12212: return 197; break;     // 'Å' - AA_capital (OZ, LA, SM)
+      //case 12212: return 192; break;   // 'À' - A accent   
+      case 1212: return 198; break;      // 'Æ' - AE_capital   (OZ, LA)
+      //case 1212: return 196; break;    // 'Ä' - A_umlaut (D, SM, OH, ...)
+      case 2222: return 138; break;      // CH  - (Russian letter symbol)
+      case 22122: return 209; break;     // 'Ñ' - (EA)               
+      //case 2221: return 214; break;    // 'Ö' – O_umlaut  (D, SM, OH, ...)
+      //case 2221: return 211; break;    // 'Ò' - O accent
+      case 2221: return 216; break;      // 'Ø' - OE_capital    (OZ, LA)
+      case 1122: return 220; break;      // 'Ü' - U_umlaut     (D, ...)
+      case 111111: return 223; break;    // beta - double S    (D?, ...)   
+      case 21211: return 199; break;   // Ç
+      case 11221: return 208; break;   // Ð
+      case 12112: return 200; break;   // È
+      case 11211: return 201; break;   // É
+      case 221121: return 142; break;  // Ž
+    #endif //OPTION_NON_ENGLISH_EXTENSIONS
 
 
-   #ifdef OPTION_NON_ENGLISH_EXTENSIONS
-     // for English/Cyrillic/Western European font LCD controller (HD44780UA02):
-     case 12212: return 197; break;     // 'Å' - AA_capital (OZ, LA, SM)
-     //case 12212: return 192; break;   // 'À' - A accent   
-     case 1212: return 198; break;      // 'Æ' - AE_capital   (OZ, LA)
-     //case 1212: return 196; break;    // 'Ä' - A_umlaut (D, SM, OH, ...)
-     case 2222: return 138; break;      // CH  - (Russian letter symbol)
-     case 22122: return 209; break;     // 'Ñ' - (EA)               
-     //case 2221: return 214; break;    // 'Ö' – O_umlaut  (D, SM, OH, ...)
-     //case 2221: return 211; break;    // 'Ò' - O accent
-     case 2221: return 216; break;      // 'Ø' - OE_capital    (OZ, LA)
-     case 1122: return 220; break;      // 'Ü' - U_umlaut     (D, ...)
-     case 111111: return 223; break;    // beta - double S    (D?, ...)   
+    //default: return 254; break;
+    default: 
+      #ifdef OPTION_UNKNOWN_CHARACTER_ERROR_TONE
+        boop();
+      #endif  //OPTION_UNKNOWN_CHARACTER_ERROR_TONE
+      return unknown_cw_character; 
+      break;
 
-
-     
-     case 21211: return 199; break;   // Ç
-     case 11221: return 208; break;   // Ð
-     case 12112: return 200; break;   // È
-     case 11211: return 201; break;   // É
-     case 221121: return 142; break;  // Ž
-   #endif //OPTION_NON_ENGLISH_EXTENSIONS
-
-
-   //default: return 254; break;
-   default: 
-     #ifdef OPTION_UNKNOWN_CHARACTER_ERROR_TONE
-       boop();
-     #endif  //OPTION_UNKNOWN_CHARACTER_ERROR_TONE
-     return unknown_cw_character; 
-     break;
-
- }
+  }
 
 }
 
--- keyer_features_and_options.h
+++ keyer_features_and_options.h
@@ -45,42 +45,42 @@
                                                              //    simultaneously.  This will make Winkey emulation be the default at boot up; 
                                                              //    hold command button down at boot up to activate CLI mode
 
-//#define OPTION_SUPPRESS_SERIAL_BOOT_MSG
+// #define OPTION_SUPPRESS_SERIAL_BOOT_MSG
 #define OPTION_INCLUDE_PTT_TAIL_FOR_MANUAL_SENDING
 #define OPTION_EXCLUDE_PTT_HANG_TIME_FOR_MANUAL_SENDING
-//#define OPTION_WINKEY_DISCARD_BYTES_AT_STARTUP     // if ASR is not disabled, you may need this to discard errant serial port bytes at startup
-//#define OPTION_WINKEY_STRICT_EEPROM_WRITES_MAY_WEAR_OUT_EEPROM // with this activated the unit will write non-volatile settings to EEPROM when set by Winkey commands
-//#define OPTION_WINKEY_SEND_WORDSPACE_AT_END_OF_BUFFER
+// #define OPTION_WINKEY_DISCARD_BYTES_AT_STARTUP     // if ASR is not disabled, you may need this to discard errant serial port bytes at startup
+// #define OPTION_WINKEY_STRICT_EEPROM_WRITES_MAY_WEAR_OUT_EEPROM // with this activated the unit will write non-volatile settings to EEPROM when set by Winkey commands
+// #define OPTION_WINKEY_SEND_WORDSPACE_AT_END_OF_BUFFER
 #define OPTION_WINKEY_STRICT_HOST_OPEN               // require an admin host open Winkey command before doing any other commands
 #define OPTION_WINKEY_2_SUPPORT                      // comment out to revert to Winkey version 1 emulation
-//#define OPTION_WINKEY_EXTENDED_COMMANDS            // in development
+// #define OPTION_WINKEY_EXTENDED_COMMANDS            // in development
 #define OPTION_WINKEY_SEND_BREAKIN_STATUS_BYTE       // additional code to check_dit_paddle() and check_dah_paddle() to send 0xC2 status byte when paddles are hit
 #define OPTION_WINKEY_INTERRUPTS_MEMORY_REPEAT
-//#define OPTION_WINKEY_2_HOST_CLOSE_NO_SERIAL_PORT_RESET  // activate this when using Winkey 2 emulation and Win-Test
-//#define OPTION_WINKEY_FREQUENT_STATUS_REPORT         // activate this to make Winkey emulation play better with RUMlog and RUMped
+// #define OPTION_WINKEY_2_HOST_CLOSE_NO_SERIAL_PORT_RESET  // activate this when using Winkey 2 emulation and Win-Test
+// #define OPTION_WINKEY_FREQUENT_STATUS_REPORT         // activate this to make Winkey emulation play better with RUMlog and RUMped
 #define OPTION_WINKEY_IGNORE_LOWERCASE               // Enable for typical K1EL Winkeyer behavior (use for SkookumLogger version 1.10.14 and prior to workaround "r" bug)
-//#define OPTION_REVERSE_BUTTON_ORDER                // This is mainly for the DJ0MY NanoKeyer http://nanokeyer.wordpress.com/
+// #define OPTION_REVERSE_BUTTON_ORDER                // This is mainly for the DJ0MY NanoKeyer http://nanokeyer.wordpress.com/
 #define OPTION_PROG_MEM_TRIM_TRAILING_SPACES         // trim trailing spaces from memory when programming in command mode
 #define OPTION_DIT_PADDLE_NO_SEND_ON_MEM_RPT         // this makes dit paddle memory interruption a little smoother
-//#define OPTION_MORE_DISPLAY_MSGS                     // additional optional display messages - comment out to save memory
-//#define OPTION_N1MM_WINKEY_TAB_BUG_WORKAROUND        // enable this to ignore the TAB key in the Send CW window (this breaks SO2R functionality in N1MM)
-//#define OPTION_WATCHDOG_TIMER                        // this enables a four second ATmega48/88/168/328 watchdog timer; use for unattended/remote operation only
-//#define OPTION_MOUSE_MOVEMENT_PADDLE               // experimental (just fooling around) - mouse movement will act like a paddle
-//#define OPTION_NON_ENGLISH_EXTENSIONS  // add support for additional CW characters (i.e. À, Å, Þ, etc.)
-//#define OPTION_KEEP_PTT_KEYED_WHEN_CHARS_BUFFERED    // this option keeps PTT high if there are characters buffered from the keyboard, the serial interface, or Winkey
-//#define OPTION_DISPLAY_NON_ENGLISH_EXTENSIONS  // LCD display suport for non-English (NO/DK/DE) characters - Courtesy of OZ1JHM
-//#define OPTION_UNKNOWN_CHARACTER_ERROR_TONE
-//#define OPTION_DO_NOT_SAY_HI
-//#define OPTION_USE_ORIGINAL_VERSION_2_1_PS2KEYBOARD_LIB //use version 2.1 PS2Keyboard.h and PS2Keyboard.cpp for FEATURE_PS2_KEYBOARD
-//#define OPTION_PS2_NON_ENGLISH_CHAR_LCD_DISPLAY_SUPPORT // makes some non-English characters from the PS2 keyboard display correctly in the LCD display (donated by Marcin sp5iou)
-//#define OPTION_PS2_KEYBOARD_RESET // reset the PS2 keyboard upon startup with 0xFF (contributed by Bill, W9BEL)
+// #define OPTION_MORE_DISPLAY_MSGS                     // additional optional display messages - comment out to save memory
+// #define OPTION_N1MM_WINKEY_TAB_BUG_WORKAROUND        // enable this to ignore the TAB key in the Send CW window (this breaks SO2R functionality in N1MM)
+// #define OPTION_WATCHDOG_TIMER                        // this enables a four second ATmega48/88/168/328 watchdog timer; use for unattended/remote operation only
+// #define OPTION_MOUSE_MOVEMENT_PADDLE               // experimental (just fooling around) - mouse movement will act like a paddle
+// #define OPTION_NON_ENGLISH_EXTENSIONS  // add support for additional CW characters (i.e. À, Å, Þ, etc.)
+// #define OPTION_KEEP_PTT_KEYED_WHEN_CHARS_BUFFERED    // this option keeps PTT high if there are characters buffered from the keyboard, the serial interface, or Winkey
+// #define OPTION_DISPLAY_NON_ENGLISH_EXTENSIONS  // LCD display suport for non-English (NO/DK/DE) characters - Courtesy of OZ1JHM
+// #define OPTION_UNKNOWN_CHARACTER_ERROR_TONE
+// #define OPTION_DO_NOT_SAY_HI
+// #define OPTION_USE_ORIGINAL_VERSION_2_1_PS2KEYBOARD_LIB //use version 2.1 PS2Keyboard.h and PS2Keyboard.cpp for FEATURE_PS2_KEYBOARD
+// #define OPTION_PS2_NON_ENGLISH_CHAR_LCD_DISPLAY_SUPPORT // makes some non-English characters from the PS2 keyboard display correctly in the LCD display (donated by Marcin sp5iou)
+// #define OPTION_PS2_KEYBOARD_RESET // reset the PS2 keyboard upon startup with 0xFF (contributed by Bill, W9BEL)
 // #define OPTION_SAVE_MEMORY_NANOKEYER
 #define OPTION_CW_KEYBOARD_CAPSLOCK_BEEP
-//#define OPTION_CW_KEYBOARD_ITALIAN
+// #define OPTION_CW_KEYBOARD_ITALIAN
 #define OPTION_CW_DECODER_GOERTZEL_AUDIO_DETECTOR
-//#define OPTION_INVERT_PADDLE_PIN_LOGIC
-//#define OPTION_DIT_DAH_BUFFERS_OFF_BY_DEFAULT_FOR_FEATURE_DIT_DAH_BUFFER_CONTROL
-//#define OPTION_ADVANCED_SPEED_DISPLAY //enables "nerd" speed visualization on display: wpm, cpm (char per min), duration of dit and dah in milliseconds and ratio (contributed by Giorgio, IZ2XBZ)
+// #define OPTION_INVERT_PADDLE_PIN_LOGIC
+// #define OPTION_DIT_DAH_BUFFERS_OFF_BY_DEFAULT_FOR_FEATURE_DIT_DAH_BUFFER_CONTROL
+// #define OPTION_ADVANCED_SPEED_DISPLAY //enables "nerd" speed visualization on display: wpm, cpm (char per min), duration of dit and dah in milliseconds and ratio (contributed by Giorgio, IZ2XBZ)
 
 
 // #define FEATURE_COMPETITION_COMPRESSION_DETECTION //(Experimental)
