k3ng/k3ng_cw_keyer

commit 984f038e31a9259fce9d3a0e51497b580075d388

Add HARDWARE_YAACWK and left not default in this branch"
This reverts commit 24624afdcea3bbb1dd9c1ebab9414a82ab379ebe.

k3ng_keyer.ino
@@ -253,6 +253,10 @@ New fetures in this stable release:
       Memories can now be programmed in commmand mode (FEATURE_COMMAND_BUTTONS) by pressing the memory button
       FEATURE_CW_DECODER now has digital input pin (cw_decoder_pin) and if OPTION_CW_DECODER_GOERTZEL_AUDIO_DETECTOR is enable, cw_decoder_audio_input_pin will work in parallel
 
+    Add Yaacwk support
+      add hardware files for YAACWK Arduino Nano V3.0 based board
+      see http://i1cra.briata.org/yaacwk/ for more info
+
     2.2.2015090801
       Fixed issue with FEATURE_CW_DECODER + OPTION_CW_DECODER_GOERTZEL_AUDIO_DETECTOR and wrong GOERTZ_SAMPLING_FREQ and GOERTZ_SAMPLES used in goertzel.h causing keyer lockups after startup
   
@@ -470,6 +474,10 @@ New fetures in this stable release:
   #include "keyer_features_and_options_open_interface.h"
 #endif
 
+#ifdef HARDWARE_YAACWK
+  #include "keyer_features_and_options_yaacwk.h"
+#endif
+
 #ifdef HARDWARE_TEST
   #include "keyer_features_and_options_test.h"
 #endif    
@@ -502,6 +510,10 @@ New fetures in this stable release:
   #include "keyer_pin_settings_open_interface.h"
   #include "keyer_settings_open_interface.h"
 #endif
+
+#ifdef HARDWARE_YAACWK
+  #include "keyer_pin_settings_yaacwk.h"
+  #include "keyer_settings_yaacwk.h"
 
 #ifdef HARDWARE_TEST
   #include "keyer_pin_settings_test.h"

keyer_hardware.h
@@ -13,11 +13,12 @@
 // #define HARDWARE_NANOKEYER_REV_D   // https://nanokeyer.wordpress.com/nanokeyer-info/  files: keyer_pin_settings_nanokeyer_rev_d.h, keyer_features_and_options_nanokeyer_rev_d.h, keyer_settings_nanokeyer_rev_d.h  
 // #define HARDWARE_OPEN_INTERFACE   // http://remoteqth.com/open-interface.php   files: keyer_pin_settings_open_interface.h, keyer_features_and_options_open_interface.h, keyer_settings_open_interface.h
 // #define HARDWARE_ARDUINO_DUE   
+// #define HARDWARE_YAACWK   // http://i1cra.briata.org/yaacwk/  files: keyer_pin_settings_yaacwk.h, keyer_features_and_options_yaacwk.h, keyer_settings_yaacwk.h
 // #define HARDWARE_TEST
 
 
 // do not touch anything below this line
 
-#if defined(HARDWARE_NANOKEYER_REV_B) || defined(HARDWARE_NANOKEYER_REV_D) || defined(HARDWARE_OPEN_INTERFACE) || defined(HARDWARE_TEST)
+#if defined(HARDWARE_NANOKEYER_REV_B) || defined(HARDWARE_NANOKEYER_REV_D) || defined(HARDWARE_OPEN_INTERFACE) || defined(HARDWARE_YAACWK) || defined(HARDWARE_TEST)
   #define HARDWARE_CUSTOM
 #endif

commit 3c25c1943d078714afb3e6e685b6fb6987548a01

fix typo caused by 984f038e31a9259fce9d3a0e51497b580075d388

k3ng_keyer.ino
@@ -514,6 +514,7 @@ New fetures in this stable release:
 #ifdef HARDWARE_YAACWK
   #include "keyer_pin_settings_yaacwk.h"
   #include "keyer_settings_yaacwk.h"
+#endif
 
 #ifdef HARDWARE_TEST
   #include "keyer_pin_settings_test.h"