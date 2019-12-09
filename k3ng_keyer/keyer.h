3f1a8ecd89cbda166d055f78d141a92c5d2dee92
VK2EFL <42091734+VK2EFL@users.noreply.github.com>
2019-12-09 11:24:34 +1100

    Update keyer.h
    Added 'SOS' prosign

--- k3ng_keyer/keyer.h
+++ k3ng_keyer/keyer.h
@@ -69,7 +69,8 @@
   #define PROSIGN_SK 135
   #define PROSIGN_SN 136
   #define PROSIGN_HH 137    // iz0rus
-  #define PROSIGN_END 138  // iz0rus
+  #define PROSIGN_SOS 138
+  #define PROSIGN_END 139  // iz0rus
 #endif
 
 #define SERIAL_SEND_BUFFER_NORMAL 0
